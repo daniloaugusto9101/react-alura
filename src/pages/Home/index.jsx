c35a4c266d3e89f728a136aca3a906752c1f9d59
Danilo Augusto <danilloagt@gmail.com>
2025-11-21 08:06:35 -0300

    refator: Ajuste nome features

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
-import { Header } from "@/feature/Header"
-import { Banner } from "@/feature/Banner"
-import { FormularioDeEvento } from "@/feature/FormularioDeEvento"
-import { Card } from "@/feature/Card"
+import { Header } from "@/features/Header"
+import { Banner } from "@/features/Banner"
+import { FormularioDeEvento } from "@/features/FormularioDeEvento"
+import { Card } from "@/features/Card"
 
 export function Home() {
   return (
